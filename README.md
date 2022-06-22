5734096a7e769871ec5bbc68cb830773fe7a6420
Sotatek-TiepLe <tiep.le@sotatek.com>
2022-06-22 08:46:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-lzyvxr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-lzyvxr)
---
+++ demo.js
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import copy from 'copy-to-clipboard';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+
+export default function TriggersTooltips() {
+  const [open, setOpen] = React.useState(false);
+  const [text, setText] = React.useState('text');
+  const [copied, setCopy] = useCopyClipboard(2000);
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+    setText('copyed');
+    setCopy('adfs');
+  };
+
+  return (
+    <div>
+      <Grid container justifyContent="center">
+        <Grid item>
+          <Tooltip disableFocusListener title="Add">
+            <Button>Hover or touch</Button>
+          </Tooltip>
+        </Grid>
+        <Grid item>
+          <Tooltip disableHoverListener title="Add">
+            <Button>Focus or touch</Button>
+          </Tooltip>
+        </Grid>
+        <Grid item>
+          <Tooltip disableFocusListener disableTouchListener title="Add">
+            <Button>Hover</Button>
+          </Tooltip>
+        </Grid>
+        <Grid item>
+          <ClickAwayListener onClickAway={handleTooltipClose}>
+            <div>
+              <Tooltip
+                PopperProps={{
+                  disablePortal: true,
+                }}
+                onClose={handleTooltipClose}
+                disableFocusListener
+                disableTouchListener
+                title={copied ? 'copied' : 'clip'}
+              >
+                <Button onClick={handleTooltipOpen}>Click</Button>
+              </Tooltip>
+            </div>
+          </ClickAwayListener>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+function useCopyClipboard(timeout = 500) {
+  const [isCopied, setIsCopied] = React.useState(false);
+
+  const staticCopy = React.useCallback((text) => {
+    const didCopy = copy(text);
+    setIsCopied(didCopy);
+  }, []);
+
+  React.useEffect(() => {
+    if (isCopied) {
+      const hide = setTimeout(() => setIsCopied(false), timeout);
+
+      return () => clearTimeout(hide);
+    }
+    return undefined;
+  }, [isCopied, setIsCopied, timeout]);
+
+  return [isCopied, staticCopy];
+}
